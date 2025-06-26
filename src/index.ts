81d268d300956acba5094057282e24b282a6d97a
Arsen Tabaku <arsentabaku2@gmail.com>
2025-06-26 12:37:15 +0200

    Implemented and tested parseOperator, parseOpenParenthesis and parseCloseParenthesis

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-import { parseNumber } from "./parsers";
+import {
+  parseNumber,
+  parseOperator,
+  parseOpenParenthesis,
+  parseCloseParenthesis,
+} from "./parsers";
 
 console.log("\n--- PARSE NUMBER ---\n");
 console.log(parseNumber("123"));
 console.log(parseNumber("1 + 2"));
 console.log(parseNumber("+ 2"));
+
+console.log("\n--- PARSE OPERATOR ---\n");
+console.log(parseOperator("+"));
+console.log(parseOperator("1 +"));
+console.log(parseOperator("- 2"));
+
+console.log("\n--- PARSE OPEN PARENTHESIS ---\n");
+console.log(parseOpenParenthesis("("));
+console.log(parseOpenParenthesis("+ ("));
+console.log(parseOpenParenthesis(")"));
+
+console.log("\n--- PARSE CLOSE PARENTHESIS ---\n");
+console.log(parseCloseParenthesis(")"));
+console.log(parseCloseParenthesis("+ )"));
+console.log(parseCloseParenthesis("()"));
--- src/parsers/parsers.ts
+++ src/parsers/parsers.ts
@@ -37,3 +37,42 @@ export const parseNumber: Parser = (input: string) => {
 
   return failure("Not a number");
 };
+
+export const parseOperator: Parser = (input: string) => {
+  const firstChar = input[0];
+
+  if (firstChar === "+" || firstChar === "-") {
+    return success(
+      [{ type: TokenTypes.OPERATOR, value: firstChar }],
+      input.slice(1)
+    );
+  }
+
+  return failure("Expected '+ or -'");
+};
+
+export const parseOpenParenthesis: Parser = (input: string) => {
+  const firstChar = input[0];
+
+  if (firstChar === "(") {
+    return success(
+      [{ type: TokenTypes.OPEN_PARENTHESIS, value: firstChar }],
+      input.slice(1)
+    );
+  }
+
+  return failure("Expected '('");
+};
+
+export const parseCloseParenthesis: Parser = (input: string) => {
+  const firstChar = input[0];
+
+  if (firstChar === ")") {
+    return success(
+      [{ type: TokenTypes.CLOSE_PARENTHESIS, value: firstChar }],
+      input.slice(1)
+    );
+  }
+
+  return failure("Expected ')'");
+};
